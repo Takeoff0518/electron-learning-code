6e2c0c85c70f533bba02d699911f746b75e22890
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2023-12-05 00:28:32 +0800

    route

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
